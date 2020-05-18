ac162aff17c3178972ee5103f503819dcae690fe
Stephen Haywood <stephen@averagesecurityguy.info>
2020-05-17 20:36:58 -0400

    Testing Updates
    Added new tests to deal with issue #7.
    Moved mechanism tests to separate file.

---
+++ mechanism_test.go
@@ -0,0 +1,53 @@
+package spf
+
+import (
+	"testing"
+)
+
+type mechtest struct {
+	raw    string
+	name   string
+	domain string
+	prefix string
+	result Result
+}
+
+
+func TestNewMechanism(t *testing.T) {
+	tests := []mechtest{
+		mechtest{"+all", "all", domain, "", Pass},
+		mechtest{"-ip6:1080::8:800:68.0.3.1", "ip6", "1080::8:800:68.0.3.1", "", Fail},
+		mechtest{"~ip6:1080::8:800:68.0.3.1/96", "ip6", "1080::8:800:68.0.3.1", "96", SoftFail},
+		mechtest{"?ip4:192.168.0.1", "ip4", "192.168.0.1", "", Neutral},
+		mechtest{"ip4:192.168.0.1/16", "ip4", "192.168.0.1", "16", Pass},
+		mechtest{"-a", "a", domain, "", Fail},
+		mechtest{"~a/24", "a", domain, "24", SoftFail},
+		mechtest{"?a:offsite.example.com", "a", "offsite.example.com", "", Neutral},
+		mechtest{"a:offsite.example.com/24", "a", "offsite.example.com", "24", Pass},
+		mechtest{"mx", "mx", domain, "", Pass},
+		mechtest{"mx/24", "mx", domain, "24", Pass},
+		mechtest{"mx:deferrals.domain.com", "mx", "deferrals.domain.com", "", Pass},
+		mechtest{"mx:deferrals.domain.com/24", "mx", "deferrals.domain.com", "24", Pass},
+		mechtest{"ptr", "ptr", domain, "", Pass},
+		mechtest{"ptr:domain.name", "ptr", "domain.name", "", Pass},
+		mechtest{"include:domain.name", "include", "domain.name", "", Pass},
+		mechtest{"exists:domain.name", "exists", "domain.name", "", Pass},
+	}
+
+	for _, expected := range tests {
+		actual := NewMechanism(expected.raw, domain)
+
+		if expected.name != actual.Name {
+			t.Error("Expected", expected.name, "got", actual.Name, ":", expected.raw)
+		}
+		if expected.domain != actual.Domain {
+			t.Error("Expected", expected.domain, "got", actual.Domain, ":", expected.raw)
+		}
+		if expected.prefix != actual.Prefix {
+			t.Error("Expected", expected.prefix, "got", actual.Prefix, ":", expected.raw)
+		}
+		if expected.result != actual.Result {
+			t.Error("Expected", expected.prefix, "got", actual.Prefix, ":", expected.raw)
+		}
+	}
+}
--- spf_test.go
+++ spf_test.go
@@ -6,14 +6,6 @@ import (
 
 const domain = "google.com"
 
-type mechtest struct {
-	raw    string
-	name   string
-	domain string
-	prefix string
-	result Result
-}
-
 type spferror struct {
 	domain string
 	raw    string
@@ -28,45 +20,6 @@ type spftest struct {
 type spfstr struct {
 	raw      string
 	expected string
-}
-
-func TestNewMechanism(t *testing.T) {
-	tests := []mechtest{
-		mechtest{"+all", "all", domain, "", Pass},
-		mechtest{"-ip6:1080::8:800:68.0.3.1", "ip6", "1080::8:800:68.0.3.1", "", Fail},
-		mechtest{"~ip6:1080::8:800:68.0.3.1/96", "ip6", "1080::8:800:68.0.3.1", "96", SoftFail},
-		mechtest{"?ip4:192.168.0.1", "ip4", "192.168.0.1", "", Neutral},
-		mechtest{"ip4:192.168.0.1/16", "ip4", "192.168.0.1", "16", Pass},
-		mechtest{"-a", "a", domain, "", Fail},
-		mechtest{"~a/24", "a", domain, "24", SoftFail},
-		mechtest{"?a:offsite.example.com", "a", "offsite.example.com", "", Neutral},
-		mechtest{"a:offsite.example.com/24", "a", "offsite.example.com", "24", Pass},
-		mechtest{"mx", "mx", domain, "", Pass},
-		mechtest{"mx/24", "mx", domain, "24", Pass},
-		mechtest{"mx:deferrals.domain.com", "mx", "deferrals.domain.com", "", Pass},
-		mechtest{"mx:deferrals.domain.com/24", "mx", "deferrals.domain.com", "24", Pass},
-		mechtest{"ptr", "ptr", domain, "", Pass},
-		mechtest{"ptr:domain.name", "ptr", "domain.name", "", Pass},
-		mechtest{"include:domain.name", "include", "domain.name", "", Pass},
-		mechtest{"exists:domain.name", "exists", "domain.name", "", Pass},
-	}
-
-	for _, expected := range tests {
-		actual := NewMechanism(expected.raw, domain)
-
-		if expected.name != actual.Name {
-			t.Error("Expected", expected.name, "got", actual.Name, ":", expected.raw)
-		}
-		if expected.domain != actual.Domain {
-			t.Error("Expected", expected.domain, "got", actual.Domain, ":", expected.raw)
-		}
-		if expected.prefix != actual.Prefix {
-			t.Error("Expected", expected.prefix, "got", actual.Prefix, ":", expected.raw)
-		}
-		if expected.result != actual.Result {
-			t.Error("Expected", expected.prefix, "got", actual.Prefix, ":", expected.raw)
-		}
-	}
 }
 
 func TestNewSPF(t *testing.T) {
@@ -89,6 +42,9 @@ func TestSPFTest(t *testing.T) {
 	tests := []spftest{
 		spftest{"127.0.0.1", "info@google.com", SoftFail},
 		spftest{"74.125.141.26", "info@google.com", Pass},
+		spftest{"35.190.247.0", "info@google.com", Pass},
+		spftest{"172.217.0.0", "info@_netblocks3.google.com", Pass},
+		spftest{"172.217.0.0", "info@google.com", Pass},
 	}
 
 	for _, expected := range tests {
@@ -98,7 +54,7 @@ func TestSPFTest(t *testing.T) {
 		}
 
 		if actual != expected.result {
-			t.Error("Expected", expected.result, "got", actual)
+			t.Error("For", expected.server, "at", expected.email, "Expected", expected.result, "got", actual)
 		}
 	}
 }
